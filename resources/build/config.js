504cbcf1c5d1d4eed84c18c117acf3a862f23db8
Cesar Denis <cesar@cesardenis.com>
2017-12-04 18:27:41 -0200

    feaure: initial webpack config

---
+++ resources/build/config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const { argv } = require('yargs');
+const merge = require('webpack-merge');
+
+const desire = require('./util/desire');
+
+const rootPath = process.cwd();
+
+const isProduction = !!((argv.env && argv.env.production) || argv.p);
+const userConfig = desire(`${__dirname}/../config`);
+
+const config = merge({
+  paths: {
+    root: rootPath,
+    assets: path.join(rootPath, 'resources/assets'),
+    dist: path.join(rootPath, 'src/public')
+  },
+  enabled: {
+    sourceMaps: !isProduction,
+    optimize: isProduction,
+    cacheBusting: isProduction,
+    watcher: !!argv.watch,
+  },
+  watch: [],
+}, userConfig);
+
+module.exports = merge(config, {
+  env: Object.assign({ production: isProduction, development: !isProduction }, argv.env),
+  publicPath: `${config.publicPath}/${path.basename(config.paths.dist)}/`,
+  manifest: {},
+});
+
+if (process.env.NODE_ENV === undefined) {
+  process.env.NODE_ENV = isProduction ? 'production' : 'development';
+}
---
+++ resources/build/webpack.config.js
@@ -0,0 +1,54 @@
+'use strict'; // eslint-disable-line
+
+const webpack = require('webpack');
+
+const config = require('./config');
+
+const assetsFilenames = (config.enabled.cacheBusting) ? config.cacheBusting : '[name]';
+
+let webpackConfig = {
+  context: config.paths.assets,
+  entry: config.entry,
+  devtool: (config.enabled.sourceMaps ? '#source-map' : undefined),
+  output: {
+    path: config.paths.dist,
+    publicPath: config.publicPath,
+    filename: `scripts/${assetsFilenames}.js`,
+  },
+  stats: {
+    hash: false,
+    version: false,
+    timings: false,
+    children: false,
+    errors: false,
+    errorDetails: false,
+    warnings: false,
+    chunks: false,
+    modules: false,
+    reasons: false,
+    source: false,
+    publicPath: false,
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.js$/,
+        include: config.paths.assets,
+        use: 'eslint'
+      },
+      {
+        test: /\.js$/,
+        exclude: [/(node_modules|bower_components)(?![/|\\](bootstrap|foundation-sites))/],
+        use: [
+          { loader: 'babel' },
+        ],
+      }
+    ]
+  },
+  resolveLoader: {
+    moduleExtensions: ['-loader'],
+  }
+};
+
+module.exports = webpackConfig;
